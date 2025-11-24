e090f269054f8d2d0214af2701e7d7556dd45006
Victoria Khoreva <victheliar@gmail.com>
2025-11-24 13:47:30 +0200

    fix coverage report generation

--- tasks.py
+++ tasks.py
@@ -9,12 +9,8 @@ def test(ctx):
     ctx.run("pytest src", pty=True)
 
 @task
-def coverage(ctx):
-    ctx.run("coverage run --branch -m pytest src", pty=True)
-
-@task
 def coverage_report(ctx):
-   ctx.run("coverage html", pty=True) 
+    ctx.run("coverage run --branch -m pytest src; coverage html", pty=True)
 
 @task
 def lint(ctx):
