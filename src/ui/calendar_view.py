54332ed40854eb48e3c854ba3585dcc951af00d6
Victoria Khoreva <victheliar@gmail.com>
2025-12-20 18:04:13 +0200

    fix calendar view

--- src/ui/calendar_view.py
+++ src/ui/calendar_view.py
@@ -57,7 +57,7 @@ class CalendarItemView:
 
     def _get_events_for_date(self, date):
         event_service._user = self._user
-        events = event_service.get_events_by_date(date, self._user.username)
+        events = event_service._event_repo.find_by_username(self._user.username)
         return [event.content for event in events if getattr(event, "date", "") == date]
 
     def _click_date(self, day, month, year):
